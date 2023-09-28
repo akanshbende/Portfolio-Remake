9e667e52f9cea7d20414bebcc9bf7b3b6c61fdc4
Akansh <akanshbende29@gmail.com>
2023-09-28 19:32:48 +0530

    bug 7

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        "venice-blue": "#30547E",
-        //orange: colors.orange,
-        primary: colors.blue,
-        secondary: colors.emerald,
-        tertiary: colors.gray,
-        danger: colors.red,
-        "code-400": "#fefcf9",
-        "code-600": "#3c455b",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
